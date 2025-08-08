449ef3959637b9ad5a472d0c6e07d7d5d32597ac
Sharan C M <cmsharan.2019@gmail.com>
2025-08-08 18:16:59 +0530

    added Download and Upload function

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,33 @@ const measurePing = async () => {
     await fetch('http://pingserver.cmsharan.com/ping',
         { method: "HEAD" }
     )
-    const end = performance.now();
-    const ping = end - start
-    console.log(`Ping: ${ping} ms`)
+    const ping = performance.now() - start;
+    console.log(`Ping: ${ping.toFixed(0)} ms`)
+}
+
+const measureDownload = async () => {
+    const start = performance.now();
+    const res = await fetch('http://pingserver.cmsharan.com/download')
+    await res.arrayBuffer();
+    const duration = performance.now() - start;
+    const speedMbps = (10 * 8) / (duration / 1000)
+    console.log(`Download Speed: ${speedMbps.toFixed(0)}`)
+}
+
+const measureUpload = async () => {
+    const buffer = new Uint8Array(10 * 1024 * 1024).fill(1)
+    const start = performance.now();
+    await fetch('http://pingserver.cmsharan.com/upload', {
+        method: 'POST',
+        body: buffer
+    });
+    const duration = performance.now() - start
+    const speedMbps = (10 * 8) / (duration / 1000)
+    console.log(`Upload Speed: ${speedMbps.toFixed(0)}`)
 }
 
 measurePing()
+measureUpload()
+measureDownload()
+
+
